561901d24a4e77ba7c132d5c161a41aeae9e02e7
tianyufighter <634522023@qq.com>
2022-03-23 21:20:15 +0800

    初始化云客系统的结构

---
+++ technicalArchive/src/main/java/com/titos/technicalarchive/TechnicalArchiveApplication.java
@@ -0,0 +1,13 @@
+package com.titos.technicalarchive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnicalArchiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnicalArchiveApplication.class, args);
+    }
+
+}
