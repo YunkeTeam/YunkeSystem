e5ba64cf39fec40d6a1599c16ba6aee543f4b46a
kurihada <63988604+kurihada@users.noreply.github.com>
2022-04-09 21:44:54 +0800

    新增查看通告模块

--- info/src/main/java/com/titos/info/shareplatform/entity/Notice.java
+++ info/src/main/java/com/titos/info/shareplatform/entity/Notice.java
@@ -1,11 +1,15 @@
 package com.titos.info.shareplatform.entity;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
 import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 /**
  * @ClassName Notice
@@ -29,5 +33,11 @@ public class Notice {
      * 通告内容
      */
     private String noticeContent;
+
+    /**
+     * 创建时间
+     */
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
 
 }
