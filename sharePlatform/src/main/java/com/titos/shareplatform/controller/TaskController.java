0d6bc3fd2d86b33d5169f68072ebee73f316b289
kurihada <63988604+kurihada@users.noreply.github.com>
2022-04-13 11:03:12 +0800

    任务查询接口优化

--- sharePlatform/src/main/java/com/titos/shareplatform/controller/TaskController.java
+++ sharePlatform/src/main/java/com/titos/shareplatform/controller/TaskController.java
@@ -1,8 +1,6 @@
 package com.titos.shareplatform.controller;
 
 import com.titos.info.global.CommonResult;
-import com.titos.info.shareplatform.vo.ConditionVO;
-import com.titos.info.shareplatform.vo.TagNameVO;
 import com.titos.info.shareplatform.vo.TaskVO;
 import com.titos.shareplatform.service.TaskService;
 import com.titos.tool.annotions.InjectToken;
@@ -43,6 +41,23 @@ public class TaskController {
     }
 
     /**
+     * 根据分类名查询任务
+     *
+     * @param customStatement 用户信息
+     * @param tagName         分类名
+     * @return 任务列表
+     */
+    @InjectToken
+    @GetMapping("/tags")
+    public CommonResult<List<TaskVO>> listTaskByTagName(
+            CustomStatement customStatement,
+            String tagName,
+            @RequestParam(defaultValue = "1", value = "pageNum") Long pageNum,
+            @RequestParam(defaultValue = "10", value = "pageSize") Long pageSize) {
+        return taskService.listTaskByTagName(customStatement, tagName, pageNum, pageSize);
+    }
+
+    /**
      * 按属性分页查询当前用户的任务
      *
      * @param customStatement 用户信息
@@ -59,23 +74,6 @@ public class TaskController {
             @RequestParam(defaultValue = "1", value = "pageNum") Long pageNum,
             @RequestParam(defaultValue = "10", value = "pageSize") Long pageSize) {
         return taskService.listTaskByCondition(customStatement, keywords, pageNum, pageSize);
-    }
-
-    /**
-     * 根据分类名查询任务
-     *
-     * @param customStatement 用户信息
-     * @param tagNameVO       分类名
-     * @return 任务列表
-     */
-    @InjectToken
-    @GetMapping("/tags")
-    public CommonResult<List<TaskVO>> listTaskByTagName(
-            CustomStatement customStatement,
-            @RequestBody TagNameVO tagNameVO,
-            @RequestParam(defaultValue = "1", value = "pageNum") Long pageNum,
-            @RequestParam(defaultValue = "10", value = "pageSize") Long pageSize) {
-        return taskService.listTaskByTagName(customStatement, tagNameVO, pageNum, pageSize);
     }
 
 }
